3d6e689cdca389d1dffebe220dddfc161d72672d
wjsthd10 <wjsthd20@naver.com>
2022-05-04 12:17:15 +0900

    2022-05-04 first commit 송윤선

---
+++ app/src/main/java/com/example/git_share_test_01/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.git_share_test_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // share git test01
+    }
+}
